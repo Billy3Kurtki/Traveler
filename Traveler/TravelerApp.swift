d9f099720a8a72998c03e97b732b0509d9284729
Kirill <kirill.kazakov26@gmail.com>
2023-02-27 22:16:10 +0300

    Initial Commit

---
+++ Traveler/TravelerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelerApp.swift
+//  Traveler
+//
+//  Created by Кирилл Казаков on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
