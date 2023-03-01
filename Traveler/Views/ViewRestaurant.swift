5e0583b84b398342d2705efa01edc258adfa4461
Kirill <kirill.kazakov26@gmail.com>
2023-03-01 22:05:43 +0300

    added description

--- Traveler/Views/ViewRestaurant.swift
+++ Traveler/Views/ViewRestaurant.swift
@@ -10,31 +10,48 @@ import SwiftUI
 struct SubViewRestaurant: View {
     @Environment(\.dismiss) var dismiss
     var restautant: Restaurant
+    @State private var showFullText = false
     var images = ["testImage", "russia-flag-xs", "testImage", "russia-flag-xs"]
     @State private var selectedImage = 0
     var body: some View {
-        VStack(spacing: 20) {
-            HStack{
-                Text(restautant.name)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                Spacer()
-            }
-            Image("testImage")
-                .resizable()
-                .frame(width: 370, height: 250)
-            HStack {
-                Text(restautant.adress)
-                Spacer()
-            }.padding(.leading, 30)
-            HStack {
-                Text(String(restautant.points!))
-                Image(systemName: "star.fill")
-                    .foregroundColor(.black)
-                Spacer()
-            }.padding(.leading, 30)
-            Spacer()
-        }.padding()
+        ScrollView {
+            VStack(spacing: 20) {
+                HStack{
+                    Text(restautant.name)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Spacer()
+                }
+                Image("testImage")
+                    .resizable()
+                    .frame(width: 370, height: 250)
+                HStack {
+                    Text(restautant.adress)
+                    Spacer()
+                }.padding(.leading, 30)
+                HStack {
+                    Text(String(restautant.points!))
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.black)
+                    Spacer()
+                }.padding(.leading, 30)
+                Text("Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь. Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки, скрытой в середине абзаца. Также все другие известные генераторы Lorem Ipsum используют один и тот же текст, который они просто повторяют, пока не достигнут нужный объём. Это делает предлагаемый здесь генератор единственным настоящим Lorem Ipsum генератором.")
+                    .onTapGesture {
+                        self.showFullText = true
+                    }
+                    .frame(width: 350, height: showFullText ? nil : 90)
+                    .truncationMode(.tail)
+                HStack {
+                    Button(action: {
+                        self.showFullText = false
+                    }) {
+                        Text("Скрыть")
+                    }.opacity(showFullText ? 1 : 0)
+                    Spacer()
+                }.padding(.leading, 10)
+                Spacer(minLength: 80)
+            }.padding()
+        }
     }
 }
 
